4e7f91fd6fe32994fab39c02de6822d2c9f02185
Nancy <nancy.090495@gmail.com>
2022-02-24 09:52:06 -0600

    made the Nav with CSS

--- src/Components/NavBar/Header.jsx
+++ src/Components/NavBar/Header.jsx
@@ -1,16 +1,50 @@
-import React from "react";
-import { HeaderWrap } from "./headerStyles";
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Header.css";
 
 export const Header = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <HeaderWrap>
-      <div className="topnav" id="topnav">
-        <a href="#home" className="active">
-          Inicio
-        </a>
-        <a href="#quienesSomos">Quienes somos</a>
-        <a href="#contacto">Contacto</a>
-      </div>
-    </HeaderWrap>
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          <img className="img-logo" src="/logoaarco.png" alt="logo" />
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Inicio
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              to="/products"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Quienes Somos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Contáctanos
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
